fc24cf4f06f8f918939c19ba278d730b75388d21
angela100 <angela100@DESKTOP-VQPESLV>
2024-06-30 20:09:55 +0200

    teraz widac wpisy na froncie

--- src/dzien2_3-frontend/src/components/Blog.vue
+++ src/dzien2_3-frontend/src/components/Blog.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">Pobierz wpisy</button>
         siema blog
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { dzien2_3_backend } from 'declarations/dzien2_3-backend/index';
+    export default {
+        data() {
+            return {
+                wpisy: []
+            }
+        },
+        methods: {
+            async pobierzWpisy() {
+                this.wpisy = await dzien2_3_backend.odczytaj_wpisy();
+            }
+        }
+    }
+</script>
