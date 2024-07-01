93af50f8479590e688261a4bbf401cace227ad43
angela100 <angela100@DESKTOP-VQPESLV>
2024-07-01 14:16:12 +0200

    mozna dodawac wpisy no froncie

--- src/dzien2_3-frontend/src/components/Blog.vue
+++ src/dzien2_3-frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="pobierzWpisy">Pobierz wpisy</button>
         siema blog
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpis">Dodaj wpis</button>
     </div>
 </template>
 
@@ -11,10 +13,14 @@ import { dzien2_3_backend } from 'declarations/dzien2_3-backend/index';
     export default {
         data() {
             return {
-                wpisy: []
+                wpisy: [],
+                nowyBlog: ''
             }
         },
         methods: {
+            async dodajWpis() {
+                await dzien2_3_backend.dodaj_wpis(this.nowyBlog);
+            },
             async pobierzWpisy() {
                 this.wpisy = await dzien2_3_backend.odczytaj_wpisy();
             }
